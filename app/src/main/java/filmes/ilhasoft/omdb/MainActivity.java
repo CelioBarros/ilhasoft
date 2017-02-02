0346b4e6dd57455958c3af684e54361209071178
CelioBarros <celio.barros.filho@gmail.com>
2017-02-02 14:39:37 -0200

    Fazendo request

--- app/src/main/java/filmes/ilhasoft/omdb/MainActivity.java
+++ app/src/main/java/filmes/ilhasoft/omdb/MainActivity.java
@@ -1,7 +1,22 @@
 package filmes.ilhasoft.omdb;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editTextMovie);
+
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Log.d("teste", String.valueOf(v.getText()));
+                    //performSearch();
+                    SearchMovie searchMovie = new SearchMovie(String.valueOf(v.getText()));
+                    searchMovie.execute();
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public class SearchMovie extends AsyncTask<String, String, String> {
+
+        HttpURLConnection urlConnection;
+        private String mUrl;
+
+        public SearchMovie(String movieName){
+            mUrl = "http://www.omdbapi.com/?type=movie&r=json&s=" + movieName;
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            StringBuilder result = new StringBuilder();
+
+            try {
+                URL url = new URL(mUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            }catch( Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+
+            Log.d("aqwui", result.toString());
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //Do something with the JSON string
+
+        }
+
     }
 }
