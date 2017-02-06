62b73289b317a04edd74bdcdf49447cb3232e00e
CelioBarros <celio.barros.filho@gmail.com>
2017-02-06 02:01:58 -0200

    Tela vertical

--- app/src/main/java/filmes/ilhasoft/omdb/MainActivity.java
+++ app/src/main/java/filmes/ilhasoft/omdb/MainActivity.java
@@ -1,10 +1,8 @@
 package filmes.ilhasoft.omdb;
 
 import android.app.Activity;
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -17,39 +15,16 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
-    String[] itemname ={
-            "Safari",
-            "Camera",
-            "Global",
-            "FireFox",
-            "UC Browser",
-            "Android Folder",
-            "VLC Player",
-            "Cold War"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*
 
-        setListAdapter(new ArrayAdapter<String>(
-                this, R.layout.list_movies,
-                R.id.Itemname,itemname));
-*/
-
-        
         EditText editText = (EditText) findViewById(R.id.editTextMovie);
 
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
